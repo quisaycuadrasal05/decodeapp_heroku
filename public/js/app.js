8175c58c165c99389df675b3969bd0c341e76440
quisaycuadrasal05 <quisaycuadrasal05>
2016-12-02 16:27:33 +0800

    goodbye

--- public/js/app.js
+++ public/js/app.js
@@ -50,7 +50,7 @@ class Component extends App{
 	ukayLayout(){
 		let html = `
 			 <nav>
-			    <div class="nav-wrapper blue darken-4">
+			    <div class="nav-wrapper purple darken-1">
 			      <a href="#" class="brand-logo yellow-text ">UKAY</a>
 			      <ul id="nav-mobile" class="right hide-on-med-and-down">
 			        <li><a href="#">Items</a></li>
@@ -84,7 +84,7 @@ class Component extends App{
 
 					</ul>
 				  </div>
-				<footer class="page-footer blue darken-4">		          
+				<footer class="page-footer purple darken-2">		          
 		          <div class="footer-copyright">
 		            <div class="container">
 		            © 2016 - Another decode App
